6ef862f2d4c5d7937377f204a46e8fd46d8a92c8
Mufeez Amjad <mufeez.amjad@outlook.com>
2024-10-13 17:39:38 -0400

    wip

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -18,12 +18,13 @@ use crossterm::{
 use futures::{FutureExt, StreamExt};
 use ratatui::backend::CrosstermBackend as Backend;
 use tokio::{
-    sync::mpsc::{self, UnboundedReceiver, UnboundedSender},
-    task::{self, JoinHandle as TokioJoinHandle},
+    sync::mpsc,
+    task::{JoinHandle as TokioJoinHandle},
 };
 use tokio_util::sync::CancellationToken;
 
 #[derive(Clone, Debug)]
+#[allow(unused)]
 pub enum Event {
     Init,
     Quit,
@@ -42,13 +43,15 @@ pub enum Event {
 pub struct Tui {
     /// The terminal instance.
     pub terminal: ratatui::Terminal<Backend<std::io::Stderr>>,
-    pub task: TokioJoinHandle<()>,
-    pub cancellation_token: CancellationToken,
-    pub event_rx: UnboundedReceiver<Event>,
-    pub event_tx: UnboundedSender<Event>,
+    /// The event handler.
+    pub event_handler: EventHandler,
+    /// The frame rate.
     pub frame_rate: f64,
+    /// The tick rate.
     pub tick_rate: f64,
+    /// Whether to enable mouse capture.
     pub mouse: bool,
+    /// Whether to enable bracketed paste.
     pub paste: bool,
 }
 
@@ -56,20 +59,15 @@ impl Tui {
     pub fn new() -> Result<Self> {
         let tick_rate = 4.0;
         let frame_rate = 60.0;
+
         let terminal = ratatui::Terminal::new(Backend::new(std::io::stderr()))?;
-        let (event_tx, event_rx) = mpsc::unbounded_channel();
-        let cancellation_token = CancellationToken::new();
-        // let event_handler = EventHandler::new(tick_rate as u64);
-        let task = tokio::task::spawn(async {});
+        let event_handler = EventHandler::new();
         let mouse = false;
         let paste = false;
 
         Ok(Self {
             terminal,
-            task,
-            cancellation_token,
-            event_rx,
-            event_tx,
+            event_handler,
             frame_rate,
             tick_rate,
             mouse,
@@ -98,76 +96,17 @@ impl Tui {
     }
 
     pub fn start(&mut self) {
-        let tick_delay = std::time::Duration::from_secs_f64(1.0 / self.tick_rate);
-        let render_delay = std::time::Duration::from_secs_f64(1.0 / self.frame_rate);
-        self.cancel();
-        self.cancellation_token = CancellationToken::new();
-        let _cancellation_token = self.cancellation_token.clone();
-        let _event_tx = self.event_tx.clone();
-        self.task = tokio::spawn(async move {
-            let mut reader = crossterm::event::EventStream::new();
-            let mut tick_interval = tokio::time::interval(tick_delay);
-            let mut render_interval = tokio::time::interval(render_delay);
-            _event_tx.send(Event::Init).unwrap();
-            loop {
-                let tick_delay = tick_interval.tick();
-                let render_delay = render_interval.tick();
-                let crossterm_event = reader.next().fuse();
-                tokio::select! {
-                  _ = _cancellation_token.cancelled() => {
-                    break;
-                  }
-                  maybe_event = crossterm_event => {
-                    match maybe_event {
-                      Some(Ok(evt)) => {
-                        match evt {
-                          CrosstermEvent::Key(key) => {
-                            if key.kind == KeyEventKind::Press {
-                              _event_tx.send(Event::Key(key)).unwrap();
-                            }
-                          },
-                          CrosstermEvent::Mouse(mouse) => {
-                            _event_tx.send(Event::Mouse(mouse)).unwrap();
-                          },
-                          CrosstermEvent::Resize(x, y) => {
-                            _event_tx.send(Event::Resize(x, y)).unwrap();
-                          },
-                          CrosstermEvent::FocusLost => {
-                            _event_tx.send(Event::FocusLost).unwrap();
-                          },
-                          CrosstermEvent::FocusGained => {
-                            _event_tx.send(Event::FocusGained).unwrap();
-                          },
-                          CrosstermEvent::Paste(s) => {
-                            _event_tx.send(Event::Paste(s)).unwrap();
-                          },
-                        }
-                      }
-                      Some(Err(_)) => {
-                        _event_tx.send(Event::Error).unwrap();
-                      }
-                      None => {},
-                    }
-                  },
-                  _ = tick_delay => {
-                      _event_tx.send(Event::Tick).unwrap();
-                  },
-                  _ = render_delay => {
-                      _event_tx.send(Event::Render).unwrap();
-                  },
-                }
-            }
-        });
+        self.event_handler.start(self.tick_rate, self.frame_rate);
     }
 
     pub fn stop(&self) -> Result<()> {
         self.cancel();
         let mut counter = 0;
-        while !self.task.is_finished() {
+        while !self.event_handler.is_finished() {
             std::thread::sleep(Duration::from_millis(1));
             counter += 1;
             if counter > 50 {
-                self.task.abort();
+                self.event_handler.abort();
             }
             if counter > 100 {
                 // log::error!("Failed to abort task in 100 milliseconds for unknown reason");
@@ -207,7 +146,7 @@ impl Tui {
     }
 
     pub fn cancel(&self) {
-        self.cancellation_token.cancel();
+        self.event_handler.cancel();
     }
 
     pub fn suspend(&mut self) -> Result<()> {
@@ -223,7 +162,7 @@ impl Tui {
     }
 
     pub async fn next(&mut self) -> Option<Event> {
-        self.event_rx.recv().await
+        self.event_handler.next().await
     }
 }
 
@@ -251,85 +190,119 @@ impl Drop for Tui {
 pub struct EventHandler {
     sender: mpsc::UnboundedSender<Event>,
     receiver: mpsc::UnboundedReceiver<Event>,
+    cancellation_token: CancellationToken,
+    task: Option<TokioJoinHandle<()>>,
 }
 
-// impl EventHandler {
-//     pub fn new(tick_rate: u64) -> Self {
-//         let tick_rate = std::time::Duration::from_millis(tick_rate);
-//
-//         let (sender, receiver) = mpsc::unbounded_channel();
-//         let _sender = sender.clone();
-//
-//         tokio::spawn(async move {
-//             let mut reader = crossterm::event::EventStream::new();
-//             let mut signals = Signals::new([SIGHUP, SIGTERM, SIGINT, SIGQUIT])
-//                 .context(anyhow!("Unable to create Signals handler."))
-//                 .unwrap();
-//
-//             let mut interval = tokio::time::interval(tick_rate);
-//             loop {
-//                 let delay = interval.tick();
-//                 let crossterm_event = reader.next().fuse();
-//                 let signal = signals.next().fuse();
-//
-//                 tokio::select! {
-//                   maybe_event = crossterm_event => {
-//                     match maybe_event {
-//                       Some(Ok(evt)) => {
-//                         match evt {
-//                           CrosstermEvent::Key(key) => {
-//                             if key.kind == KeyEventKind::Press {
-//                               _sender.send(Event::Key(key)).unwrap();
-//                             }
-//                           },
-//                           CrosstermEvent::Mouse(mouse) => {
-//                             _sender.send(Event::Mouse(mouse)).unwrap();
-//                           },
-//                           CrosstermEvent::Resize(x, y) => {
-//                             _sender.send(Event::Resize(x, y)).unwrap();
-//                           },
-//                           CrosstermEvent::FocusLost => {
-//                             _sender.send(Event::FocusLost).unwrap();
-//                           },
-//                           CrosstermEvent::FocusGained => {
-//                             _sender.send(Event::FocusGained).unwrap();
-//                           },
-//                           CrosstermEvent::Paste(s) => {
-//                             _sender.send(Event::Paste(s)).unwrap();
-//                           },
-//                           _ => {},
-//                         }
-//                       }
-//                       Some(Err(_)) => {}
-//                       None => {},
-//                     }
-//                   },
-//                   maybe_signal = signal => {
-//                     match maybe_signal {
-//                       Some(s) => {
-//                         match s {
-//                             SIGTERM | SIGINT | SIGQUIT => {
-//                                 _sender.send(Event::Quit).unwrap()
-//                             }
-//                             _ => {
-//                                 _sender.send(Event::Closed).unwrap()
-//                             }
-//                         }
-//                       }
-//                       None => {},
-//                     }
-//                   },
-//                   _ = delay => {
-//                       _sender.send(Event::Tick).unwrap();
-//                   },
-//                 }
-//             }
-//         });
-//
-//         Self { sender, receiver }
-//     }
-//
-//     pub async fn next(&mut self) -> Option<Event> {
-//         self.receiver.recv().await
-//     }
-// }
+impl EventHandler {
+    pub fn new() -> Self {
+        let (sender, receiver) = mpsc::unbounded_channel();
+        let cancellation_token = CancellationToken::new();
+
+        Self {
+            sender,
+            receiver,
+            cancellation_token,
+            task: None,
+        }
+    }
+
+    pub fn start(&mut self, tick_rate: f64, frame_rate: f64) {
+        let tick_delay = std::time::Duration::from_secs_f64(1.0 / tick_rate);
+        let render_delay = std::time::Duration::from_secs_f64(1.0 / frame_rate);
+
+        let _sender = self.sender.clone();
+
+        let cancellation_token = self.cancellation_token.clone();
+
+        self.task = Some(tokio::spawn(async move {
+            let mut reader = crossterm::event::EventStream::new();
+            let mut tick_interval = tokio::time::interval(tick_delay);
+            let mut render_interval = tokio::time::interval(render_delay);
+
+            // Send an init event to the receiver
+            _sender.send(Event::Init).unwrap();
+
+            loop {
+                let tick_delay = tick_interval.tick();
+                let render_delay = render_interval.tick();
+
+                let mut signals = Signals::new(&[SIGHUP, SIGTERM, SIGINT, SIGQUIT]).unwrap();
+
+                let crossterm_event = reader.next().fuse();
+                tokio::select! {
+                    _ = cancellation_token.cancelled() => {
+                        break;
+                    }
+                    maybe_event = crossterm_event => {
+                        match maybe_event {
+                            Some(Ok(evt)) => {
+                                match evt {
+                                    CrosstermEvent::Key(key) => {
+                                        if key.kind == KeyEventKind::Press {
+                                            _sender.send(Event::Key(key)).unwrap();
+                                        }
+                                    },
+                                    CrosstermEvent::Mouse(mouse) => {
+                                        _sender.send(Event::Mouse(mouse)).unwrap();
+                                    },
+                                    CrosstermEvent::Resize(x, y) => {
+                                        _sender.send(Event::Resize(x, y)).unwrap();
+                                    },
+                                    CrosstermEvent::FocusLost => {
+                                        _sender.send(Event::FocusLost).unwrap();
+                                    },
+                                    CrosstermEvent::FocusGained => {
+                                        _sender.send(Event::FocusGained).unwrap();
+                                    },
+                                    CrosstermEvent::Paste(s) => {
+                                        _sender.send(Event::Paste(s)).unwrap();
+                                    },
+                                }
+                            }
+                            Some(Err(_)) => {
+                                _sender.send(Event::Error).unwrap();
+                            }
+                            None => {},
+                        }
+                    },
+                    _ = tick_delay => {
+                        _sender.send(Event::Tick).unwrap();
+                    },
+                    _ = render_delay => {
+                        _sender.send(Event::Render).unwrap();
+                    },
+                    maybe_signal = signals.next() => {
+                        match maybe_signal {
+                            Some(SIGTERM) | Some(SIGINT) | Some(SIGQUIT) | Some(SIGHUP) => {
+                                _sender.send(Event::Quit).unwrap();
+                            },
+                            _ => unreachable!(),
+                        }
+                    },
+                }
+            }
+        }));
+    }
+
+    pub async fn next(&mut self) -> Option<Event> {
+        self.receiver.recv().await
+    }
+
+    pub fn cancel(&self) {
+        self.cancellation_token.cancel();
+    }
+
+    pub fn is_finished(&self) -> bool {
+        match self.task {
+            Some(ref task) => task.is_finished(),
+            None => true,
+        }
+    }
+
+    pub fn abort(&self) {
+        if let Some(ref task) = self.task {
+            task.abort();
+        }
+    }
+}
